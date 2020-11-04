62948ba5823aa85e4894e92940703eb7183c054e
KennethMelendez <KennethMelendez@protonmail.com>
2020-11-03 20:52:56 -0500

    demo

---
+++ ConfigApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConfigApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                        .AddJsonFile("appsettings.json", false)
+                        .Build();
+            var thing = configuration.GetSection("EmailAddress").Value;
+            Console.WriteLine(thing);
+
+        }
+
+    }
+}
